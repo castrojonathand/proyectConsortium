ec41b08c1f0b5207722f9b73b8653d9fd8243499
Castro <castrojonathand@gmail.com>
2023-10-10 07:07:20 -0300

    connecting Mongo

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,5 +1,12 @@
 import {NextResponse} from 'next/server'
+import {connectDB} from '@/Utils/dbConnect'
 
 export function GET(){
-    return NextResponse.json({ message:"Hello" })
+    
+    connectDB()
+    return NextResponse.json({ message:"it is a GET request" })
+}
+
+export function POST(){
+    return NextResponse.json({ message:"it is a POST request" })
 }
