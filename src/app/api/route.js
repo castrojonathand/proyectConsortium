02ddc6e1c59c8c43b5585b1fac19fe17ec163e1f
Castro <castrojonathand@gmail.com>
2023-10-15 04:33:01 -0300

    changes

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,12 +3,14 @@ import {connectDB} from '@/Utils/dbConnect'
 import edificios from '@/Utils/edificios'
 
 export async function GET(){  
-    connectDB()  
+    connectDB()
     const edif = await edificios.find()
     return NextResponse.json(edif)
 }
 
 export function POST(){
     connectDB()
+    
+
     return NextResponse.json({ message:"it is a POST request" })
 }
