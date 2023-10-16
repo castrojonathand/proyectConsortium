2bf3f87601e0931394ed10872c2bfe40a1e13888
Castro <castrojonathand@gmail.com>
2023-10-16 18:51:17 -0300

    add collection models

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,14 +3,23 @@ import {connectDB} from '@/Utils/dbConnect'
 import edificios from '@/Utils/edificios'
 
 export async function GET(){  
-    
+
     connectDB()
     const edif = await edificios.find()
     return NextResponse.json(edif)
 }
 
-export function POST(){
+export async function POST(request){
 
-    connectDB()  
+    try {
+        const data = await request.json()
+        
+        
+        
+    } catch (error) {
+        
+    }
+
+
     return NextResponse.json({ message:"it is a POST request" })
 }
