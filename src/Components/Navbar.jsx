2a3e5826855a496a9d73cc07a7e3d4aff3677b44
Castro <castrojonathand@gmail.com>
2023-10-23 19:15:17 -0300

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <ul className="hidden lg:flex items-center gap-12 font-poppins">
+          <li className="text-lg link-item">
+            <Link href="/rent">Rent</Link>
+          </li>
+          <li className="text-lg link-item">
+            <Link href="/fleet">Fleet</Link>
+          </li>
+          <li className="text-lg link-item">
+            <Link href="/services">Services</Link>
+          </li>
+
+          {user && (
+            <li className="text-lg link-item">
+              <Link href="/favorites">Favorites</Link>
+            </li>
+          )}  
+          
+          <li className="text-lg link-item">
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
     )
 }
 
