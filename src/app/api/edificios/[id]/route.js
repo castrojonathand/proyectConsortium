00ac3c0c6052f8b132ca8cd34c0af1b575ff266c
Castro <castrojonathand@gmail.com>
2023-10-25 14:27:38 -0300

    api findbyid

--- src/app/api/edificios/[id]/route.js
+++ src/app/api/edificios/[id]/route.js
@@ -1,15 +1,31 @@
 import { NextResponse } from "next/server";
-// import { connectDB } from "@/Utils/dbConnect";
+import { connectDB } from "@/Utils/dbConnect";
+import Edificios from "@/Models/Edificios";
 
-// connectDB();
+connectDB();
 
 export async function GET(request,{ params }) {
-    const id  = params.id;
 
-    if (!id) {
-        return NextResponse.error('Parámetro "id" no encontrado', { status: 400 });
+    try {
+        const idEdificio = await Edificios.findById(params.id)
+        if (!idEdificio) {
+            return NextResponse.json({
+                message: `Edificio no encontrado`
+            },{
+                status : 404
+            }) 
+        }    
+        return NextResponse.json(idEdificio);
+        
+    } catch (error) {
+
+        return NextResponse.json({
+            message: `Error al obtener el edificio`
+        },{
+            status : 500
+        })
+        
     }
-    return NextResponse.json({ message: `GET ${id}` });
 }
 
 
