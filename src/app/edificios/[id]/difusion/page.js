67f4640280abb3c3d48621173edd7d3eabfea503
Castro <castrojonathand@gmail.com>
2023-10-30 04:54:23 -0300

    Save Data

--- src/app/edificios/[id]/difusion/page.js
+++ src/app/edificios/[id]/difusion/page.js
@@ -5,8 +5,11 @@ import { readXlsxFile } from 'read-excel-file'
 const Difusion = () => {
     return (        
         <>
-            <div>Hola Difusion</div>
-            <ReadExcel />        
+            <div className="flex justify-between p-2 text-center">            
+                <div className='p-2'>Difusion</div>
+                <button className="btn btn-primary bg-[#4F81BD] p-2 text-white rounded-md">Cargar Datos</button>
+            </div>     
+            <ReadExcel />   
         </>
 
     )
