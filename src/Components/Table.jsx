27c5f312005664ac490c581b221d76625e9f1d99
Castro <castrojonathand@gmail.com>
2023-10-30 02:53:39 -0300

    Styles in table

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -8,26 +8,38 @@ const Table = ({ data }) => {
     const headers = Object.keys(data[0]);
 
     return (
-        <div>
-        <table>
-            <thead>
-            <tr>
-                {headers.map((header, index) => (
-                <th key={index}>{header}</th>
-                ))}
-            </tr>
-            </thead>
-            <tbody>
-            {data.map((item, rowIndex) => (
-                <tr key={rowIndex}>
-                {headers.map((header, columnIndex) => (
-                    <td key={columnIndex}>{item[header]}</td>
-                ))}
+        <div className="overflow-x-auto mt-2">
+            <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+                <thead className="bg-gray-800 text-white">
+                <tr>
+                    {headers.map((header, index) => (
+                    <th
+                        key={index}
+                        className="px-4 py-2 text-left"
+                    >
+                        {header}
+                    </th>
+                    ))}
                 </tr>
-            ))}
-            </tbody>
-        </table>
-        </div>
+                </thead>
+                <tbody className="bg-gray-100">
+                {data.map((item, rowIndex) => (
+                    <tr key={rowIndex}>
+                    {headers.map((header, columnIndex) => (
+                        <td
+                        key={columnIndex}
+                        className="px-4 py-2 border-b border-gray-300"
+                        >
+                        {item[header]}
+                        </td>
+                    ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+</div>
+
+        
     );
 };
 
