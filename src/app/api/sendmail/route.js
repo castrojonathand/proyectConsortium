1a7de4228efba517f259e0d991ed71fdb7376dab
Castro <castrojonathand@gmail.com>
2023-11-10 15:10:21 -0300

    changes attachments

--- src/app/api/sendmail/route.js
+++ src/app/api/sendmail/route.js
@@ -1,11 +1,9 @@
 import {transporter} from '@/Utils/sendEmails';
 import { NextResponse } from 'next/server';
-import {connectDB} from '@/Utils/dbConnect'
-
-connectDB()
+// import {connectDB} from '@/Utils/dbConnect'
+// connectDB()
 
 export async function POST(request){
-    console.log("POST")
     const {name,Gmail} = await request.json()  
     console.log("name>>>",name)
     console.log("Gmail>>>",Gmail)
@@ -19,7 +17,8 @@ export async function POST(request){
             subject:"Prueba de envio CV adjunto",
             text:`Hola ${name}, este es un mensaje de prueba con adjunto enviado desde el servidor de nextjs`,
             attachments:{                          
-                path: 'C:/Users/jony/Desktop/Kaba/consortium/Files/CV.pdf'                
+                path: 'C:/Users/jony/Desktop/Kaba/consortium/Files/CV.pdf',
+                href: "https://drive.google.com/file/d/1GsrPR7Mexes2Z1JKDXsUjj_Kq2irWvtV/view?usp=sharing"                   
             },
         })
         console.log("envio exitoso",response)
