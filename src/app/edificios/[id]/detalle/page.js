99c2dcf198a822e194f783deb638c4c5935cb686
Castro <castrojonathand@gmail.com>
2023-11-14 20:01:10 -0300

    Add Sonner toast notifications to edificios detalle page

--- src/app/edificios/[id]/detalle/page.js
+++ src/app/edificios/[id]/detalle/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client"
+import { toast, Toaster } from "sonner"
 
 const Detalle = () => {
   return (
-    <div>Detalle</div>
+    <div className="flex-col">
+      <h1>Detalle</h1>
+      <Toaster richColors />
+      <button onClick={ ()=> toast.success("Event has been created")}>
+        <span>Click me success</span>
+      </button>
+      <button onClick={ ()=> toast.error("Event has been created")}>
+        <span>Click me error</span>
+      </button>
+    </div>
   )
 }
-
 export default Detalle
