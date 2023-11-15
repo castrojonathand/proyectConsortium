bf3f7cf3425af48f7828cc1c4c84a67167e10f68
Castro <castrojonathand@gmail.com>
2023-11-14 21:43:26 -0300

    Refactor toast buttons and add styles

--- src/app/edificios/[id]/detalle/page.js
+++ src/app/edificios/[id]/detalle/page.js
@@ -3,14 +3,14 @@ import { toast, Toaster } from "sonner"
 
 const Detalle = () => {
   return (
-    <div className="flex-col">
+    <div className="flex-col p-2 flex">
       <h1>Detalle</h1>
       <Toaster richColors />
-      <button onClick={ ()=> toast.success("Event has been created")}>
+      <button className="btn btn-primary w-fit bg-[#4F81BD] p-2 my-1 text-white rounded-md" onClick={ ()=> toast.success("Event has been created")}>
         <span>Click me success</span>
       </button>
-      <button onClick={ ()=> toast.error("Event has been created")}>
-        <span>Click me error</span>
+      <button className="btn btn-primary w-fit bg-[#4F81BD] p-2 my-1 text-white rounded-md" onClick={ ()=> toast.error("Event has been created")}>
+        <span>Click me error lorem</span>
       </button>
     </div>
   )
